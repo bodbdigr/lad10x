e6ba1b3212119feed2b8b457593a88a9585c9d85
bodbdigr <valery@rebelmouse.com>
2022-01-28 11:22:05 +0100

    experiment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+
+import { transform, transformFromAst } from '@babel/standalone'
+import babelPluginSyntaxJsx from '@babel/plugin-syntax-jsx'
+import { declare } from '@babel/helper-plugin-utils'  
+
+const doWalk = declare(api => {
+  api.assertVersion(7)
+  return {
+    visitor: {
+      JSXElement: {
+        enter(path) {
+          path.node.openingElement.attributes.push({
+            type: "JSXAttribute",
+            name: {
+              type: "JSXIdentifier",
+              name: "__id"
+            },
+            value: {
+              type: "StringLiteral",
+              value: "abc"
+            }
+          })
+        }
+      }
+    }
+  }
+})
 
 function App() {
+  const code = `import * as React from "react"
+import { Header } from "./modules/header.js";
+
+export const App = ({ param }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div>And I'm a two</div>
+      <div>I'm a one</div>
+      <Header text={param} />
+    </React.Fragment>
+  )
+}
+`;
+
+  const { ast } = transform(code, {
+    // presets: ["es2015", "react"],
+    plugins: [babelPluginSyntaxJsx, doWalk],
+    ast: true
+  }
+)
+
+  console.log(ast);
+
+  const codeAfter = transformFromAst(ast, "", {plugins: [babelPluginSyntaxJsx]})
+
+  console.log('after', codeAfter.code)
+
+  // const res = new Function(codeAfter.code)
+  // console.log(res())
+
+  return <>text</>
 }
 
 export default App;
